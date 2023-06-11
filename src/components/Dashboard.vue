f670df4bee2a89fcc1890fe5aa685913a7370cef
PUAJINGSHENG <puajingsheng2001@gmail.com>
2023-06-11 18:15:34 +0800

    Commit changes

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -6,9 +6,10 @@
         </v-col>
         <v-col cols="12" lg="10">
           <h1>Type your words:</h1>
-          <v-textarea id="textarea-no-resize" placeholder="Type the words" rows="10" no-resize bg-color="#C0CAF1"
-            v-model="text"> </v-textarea>
+          <v-textarea id="textarea-no-resize" placeholder="Type the words" rows="10"  outlined no-resize background-color="#C0CAF1"  border-radius=8px   
+            v-model="text"  @input="calculateWordCount"> </v-textarea>
           <p :style="{ color: wordCountColor }">Word Count: {{ wordCount }}</p>
+         
         </v-col>
       </v-row>
       <v-row>
@@ -35,48 +36,55 @@
         <v-col cols="12" lg="6">
 
           Play Speed：
-          <v-slider color="#C0CAF1" :ticks="tickLabels" :max="2" step="0.5" show-ticks="always" tick-size="4"
+          <v-slider color="#C0CAF1" :ticks="ticks" :tick-labels="speedLabel" max="2" step="0.5" show-ticks="always" tick-size="4" 
             v-model="sliderValue"></v-slider>
-          <p>{{ sliderValueLabel }}</p>
-
+          <p>{{ sliderValue}}</p>
+          
         </v-col>
         <v-col cols="12" lg="6">
-          <v-switch v-model="ex11" label="Neural Speech" color="warning" value="Neural Speech" hide-details></v-switch>
+          <v-switch v-model="ex11" label="Neural Speech" color="info" value="Neural Speech" hide-details></v-switch>
         </v-col>
         <v-col cols="12" lg="6">
           <v-progress-linear :value="currentMinutes" :max="totalMinutes" color="primary"></v-progress-linear>
           <p>{{ currentMinutes }} / {{ totalMinutes }} minutes</p>
 
         </v-col>
-        <v-col cols="12" lg="6">
+        <!-- <v-col cols="12" lg="6">
 
-        </v-col>
+        </v-col> -->
       </v-row>
       <v-row>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-rewind"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon><v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon  color="#FFFFFF" >mdi-rewind</v-icon></v-avatar></v-btn> </template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-play"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon> <v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF" >mdi-play</v-icon></v-avatar></v-btn></template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-stop"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon> <v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF" >mdi-stop</v-icon></v-avatar></v-btn></template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-fast-forward"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon><v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF">mdi-fast-forward</v-icon></v-avatar></v-btn></template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-replay"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon> <v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF"> mdi-replay</v-icon></v-avatar></v-btn></template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-volume-high"></v-icon></v-avatar>
+          <v-hover> <template v-slot="{ hover }"> <v-btn icon> <v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF" >mdi-volume-high</v-icon></v-avatar> </v-btn></template>
+    </v-hover>
         </v-col>
         <v-col cols="1">
-          <v-avatar size="38" color="#000000"><v-icon color="warning" icon="mdi-download"></v-icon></v-avatar>
-
+          <v-hover> <template v-slot="{ hover }">  <v-btn icon>  <v-avatar :size="38"  :color="hover ? 'primary' : 'info'"><v-icon color="#FFFFFF" >mdi-download</v-icon></v-avatar></v-btn></template>
+    </v-hover>
+          
         </v-col>
-        <v-col cols="12" lg="5">
-        </v-col>
+        <!-- <v-col cols="12" lg="5">
+        </v-col> -->
       </v-row>
 
     </v-col>
@@ -84,21 +92,34 @@
   </v-row>
 </template>
 
+
 <script>
 import AWS from 'aws-sdk';
 
 export default {
   data() {
     return {
-      text: '',
+      text: "",
+      wordCount: 0,
+      wordCountColor: 'black',
+   
+      sliderValue: 1,
+   
+      speedLabel: [
+        '0',
+        '0.5',
+        '1',
+        '1.5',
+        '2.0',
+      ],
     };
   },
   methods: {
     speak() {
       const polly = new AWS.Polly({
-        accessKeyId: '',
-        secretAccessKey: '',
-        region: '',
+        accessKeyId: "",
+        secretAccessKey: "",
+        region: "",
       });
       const params = {
         OutputFormat: 'mp3',
@@ -115,6 +136,21 @@ export default {
         }
       });
     },
+    calculateWordCount() {
+    const words = this.text.trim().split(' ');
+
+    if (words.length === 1 && words[0] === '') {
+      this.wordCount = 0;
+    } else {
+      this.wordCount = words.length;
+    }
+
+    if (this.wordCount > 5) {
+      this.wordCountColor = 'red';
+    } else {
+      this.wordCountColor = 'black';
+    }
+  },
   },
 };
 </script>
