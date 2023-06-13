c8510582d9f7f00e62965214244de0f331cefb6e
Sarween <sarweenalages@gmail.com>
2023-06-13 17:20:56 +0800

    Logo picture added & Integrated Frontend

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,31 @@
 <template>
-  <v-row>
-    <v-col cols="12" color="primary">
+  <v-row class="purple lighten-5 pb-10">
+    <v-col cols="12" class="purple lighten-2 d-flex pa-3 mb-10 shadow1">
       <v-row>
-        <v-col cols="12" lg="1"> </v-col>
-        <v-col cols="12" lg="10">
-          <h1>Type your words:</h1>
+        <v-col cols="2" class="align-self-center">
+          <v-img
+            max-height="70"
+            max-width="120"
+            :src="require('@/assets/logo2.png')"
+            class="ml-10"
+          ></v-img>
+        </v-col>
+        <v-col cols="9"></v-col>
+        <v-col cols="1" class="align-self-center"
+          ><a href="login.vue">
+            <v-avatar color="indigo">
+              <v-icon color="#FFFFFF">mdi-logout-variant</v-icon>
+            </v-avatar></a
+          ></v-col
+        >
+      </v-row>
+    </v-col>
+
+    <v-col cols="6" class="pt-10">
+      <v-row>
+        <v-col cols="2"></v-col>
+        <v-col cols="10">
+          <h1 class="textShade">Type your words:</h1>
           <v-textarea
             id="textarea-no-resize"
             placeholder="Type the words"
@@ -12,17 +33,25 @@
             outlined
             no-resize
             background-color="#C0CAF1"
-            border-radius="8px"
+            border-radius="10px"
             v-model="text"
             @input="calculateWordCount"
+            class="mt-10"
           >
           </v-textarea>
-          <p :style="{ color: wordCountColor }">Word Count: {{ wordCount }}</p>
+
+          <h3 :style="{ color: wordCountColor }">
+            Word Count: {{ wordCount }}
+          </h3>
         </v-col>
       </v-row>
-      <v-row>
+    </v-col>
+
+    <v-col cols="6" color="primary">
+      <v-row class="pt-10 mb-5">
+        <v-col cols="1"></v-col>
         <v-col cols="12" lg="6">
-          <p>Language and Region:</p>
+          <h3>Language and Region:</h3>
           <v-combobox
             color="green"
             label="Language:"
@@ -66,24 +95,29 @@
             ]"
           ></v-combobox>
         </v-col>
-        <v-col cols="12" lg="3">
+        <!-- <v-col cols="12" lg="3">
           Voice:
           <v-radio-group inline>
             <v-radio label="Male" value="Male"></v-radio>
             <v-radio label="Female" value="Female"></v-radio>
           </v-radio-group>
-        </v-col>
+        </v-col> -->
 
-        <v-col cols="12" lg="3">
-          <v-btn color="info" @click="speak"
-            >Convert<v-icon icon="mdi-play"></v-icon
-          ></v-btn>
+        <v-col cols="12" lg="2" class="d-flex ml-auto flex-column">
+          <v-div class="d-flex align-end flex-column">
+            <v-btn :disabled="wordExceed" color="info" @click="speak"
+              >Convert</v-btn
+            >
+          </v-div>
         </v-col>
         <audio ref="audio"></audio>
+        <v-col cols="2"></v-col>
       </v-row>
-      <v-row>
+
+      <v-row class="mb-5">
+        <v-col cols="1"> </v-col>
         <v-col cols="12" lg="6">
-          Play Speed：
+          <h3>Play Speed：</h3>
           <v-slider
             color="#C0CAF1"
             :ticks="ticks"
@@ -95,17 +129,25 @@
             v-model="sliderValue"
           ></v-slider>
         </v-col>
-        <v-col cols="12" lg="6">
-          <v-switch
-            v-model="ex11"
-            value="Neural Speech"
-            label="Neural Speech"
-            color="info"
-            hide-details
-            @click="getNeural"
-          ></v-switch>
+
+        <v-col cols="12" lg="3">
+          <v-div class="d-flex align-end flex-column">
+            <v-switch
+              class="d-flex align-end flex-column"
+              v-model="ex11"
+              label="Neural Speech"
+              color="info"
+              value="Neural Speech"
+              hide-details
+            ></v-switch>
+          </v-div>
         </v-col>
-        <v-col cols="12" lg="6">
+        <v-col cols="2"></v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="12" lg="9" class="mb-5">
           <v-progress-linear
             :value="(currentSeconds / totalSeconds) * 100"
             :max="totalSeconds"
@@ -113,96 +155,101 @@
           ></v-progress-linear>
           <p>{{ currentSeconds }} / {{ totalSeconds }} seconds</p>
         </v-col>
-        <!-- <v-col cols="12" lg="6">
 
-        </v-col> -->
-      </v-row>
-      <v-row>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-btn icon @click="rewind">
-                <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
-                  <v-icon color="#FFFFFF">mdi-rewind</v-icon>
-                </v-avatar>
-              </v-btn>
-            </template>
-          </v-hover>
-        </v-col>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-btn icon @click="resume">
-                <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
-                  <v-icon color="#FFFFFF">mdi-play</v-icon>
-                </v-avatar>
-              </v-btn>
-            </template>
-          </v-hover>
-        </v-col>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-btn icon @click="stop">
-                <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
-                  <v-icon color="#FFFFFF">mdi-stop</v-icon>
-                </v-avatar>
-              </v-btn>
-            </template>
-          </v-hover>
-        </v-col>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-btn icon @click="fastForward">
-                <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
-                  <v-icon color="#FFFFFF">mdi-fast-forward</v-icon>
-                </v-avatar>
-              </v-btn>
-            </template>
-          </v-hover>
-        </v-col>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-menu offset-y>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on">
-                    <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
-                      <v-icon color="#FFFFFF">mdi-volume-high</v-icon>
-                    </v-avatar>
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-list>
-                    <v-subheader>Volume</v-subheader>
-                    <v-slider
-                      v-model="volumeValue"
-                      min="0"
-                      max="1"
-                      step="0.01"
-                      thumb-label
-                      @input="updateVolume(volumeValue)"
-                    ></v-slider>
-                  </v-list>
-                </v-card>
-              </v-menu>
-            </template>
-          </v-hover>
-        </v-col>
-        <v-col cols="1">
-          <v-hover>
-            <template v-slot="{ hover }">
-              <v-btn icon v-if="audioSrc" @click="download">
-                <v-avatar :size="38" :color="hover ? 'primary' : 'info'"
-                  ><v-icon color="#FFFFFF">mdi-download</v-icon></v-avatar
-                ></v-btn
-              ></template
-            >
-          </v-hover>
-        </v-col>
-        <!-- <v-col cols="12" lg="5">
-        </v-col> -->
+        <v-col cols="2"></v-col>
+
+        <v-row>
+          <v-col cols="2"></v-col>
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-btn icon @click="rewind">
+                  <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                    <v-icon color="#FFFFFF">mdi-rewind</v-icon>
+                  </v-avatar>
+                </v-btn>
+              </template>
+            </v-hover>
+          </v-col>
+
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-btn icon @click="resume">
+                  <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                    <v-icon color="#FFFFFF">mdi-play</v-icon>
+                  </v-avatar>
+                </v-btn>
+              </template>
+            </v-hover>
+          </v-col>
+
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-btn icon @click="stop">
+                  <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                    <v-icon color="#FFFFFF">mdi-pause</v-icon>
+                  </v-avatar>
+                </v-btn>
+              </template>
+            </v-hover>
+          </v-col>
+
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-btn icon @click="fastForward">
+                  <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                    <v-icon color="#FFFFFF">mdi-fast-forward</v-icon>
+                  </v-avatar>
+                </v-btn>
+              </template>
+            </v-hover>
+          </v-col>
+
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-menu offset-y>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on">
+                      <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                        <v-icon color="#FFFFFF">mdi-volume-high</v-icon>
+                      </v-avatar>
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-list>
+                      <v-subheader>Volume</v-subheader>
+                      <v-slider
+                        v-model="volumeValue"
+                        min="0"
+                        max="1"
+                        step="0.01"
+                        thumb-label
+                        @input="updateVolume(volumeValue)"
+                      ></v-slider>
+                    </v-list>
+                  </v-card>
+                </v-menu>
+              </template>
+            </v-hover>
+          </v-col>
+
+          <v-col cols="1">
+            <v-hover>
+              <template v-slot="{ hover }">
+                <v-btn icon v-if="audioSrc" @click="download">
+                  <v-avatar :size="38" :color="hover ? 'primary' : 'info'"
+                    ><v-icon color="#FFFFFF">mdi-download</v-icon></v-avatar
+                  ></v-btn
+                ></template
+              >
+            </v-hover>
+          </v-col>
+          <v-col cols="3"></v-col>
+        </v-row>
       </v-row>
     </v-col>
   </v-row>
@@ -225,6 +272,9 @@ export default {
       showVolumeSlider: false,
       audio: new Audio(),
       volumeValue: 1,
+      totalSeconds: 0,
+      currentSeconds: 0,
+      wordExceed: true,
     };
   },
 
@@ -280,12 +330,15 @@ export default {
           audio.volume = this.volumeValue; // Set the initial volume
           audio.play();
           this.audioSrc = url;
+          audio.addEventListener("loadedmetadata", () => {
+            this.totalSeconds = Math.floor(audio.duration);
+          });
+          audio.addEventListener("timeupdate", () => {
+            this.currentSeconds = Math.floor(audio.currentTime);
+          });
           this.$refs.audio = audio; // Assign the audio element to $refs
         }
       });
-      const words = this.text.trim().split(" ");// Adjust this value based on your desired speech rate
-
-      this.totalSeconds = Math.ceil(words.length * averageSecondsPerWord);
     },
     download() {
       if (this.audioSrc) {
@@ -297,17 +350,18 @@ export default {
     },
     calculateWordCount() {
       const words = this.text.trim().split(" ");
-
       if (words.length === 1 && words[0] === "") {
         this.wordCount = 0;
       } else {
         this.wordCount = words.length;
       }
 
-      if (this.wordCount > 5) {
+      if (this.wordCount > 250 || this.wordCount < 1) {
         this.wordCountColor = "red";
+        this.wordExceed = true;
       } else {
         this.wordCountColor = "black";
+        this.wordExceed = false;
       }
     },
     updatePlaybackSpeed() {
@@ -323,3 +377,13 @@ export default {
   },
 };
 </script>
+
+<style>
+.shadow1 {
+  box-shadow: 5px 5px 10px gray;
+}
+
+.textShade {
+  text-shadow: 2px 2px;
+}
+</style>
