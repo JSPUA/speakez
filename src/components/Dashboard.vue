158915ced5479d4c0dcb62b7d8ba9b6b35d12150
Thaya19 <thaya23580@gmail.com>
2023-06-19 05:23:08 +0800

    Final version
    Everything is working.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="web-ui" v-if="isWeb">
-      <v-row class="purple lighten-5 pb-10">
+      <v-row class="background pb-10">
         <v-col cols="12" class="purple lighten-2 d-flex pa-3 mb-10 shadow1">
           <v-row>
             <v-col cols="2" class="align-self-center">
@@ -46,15 +46,25 @@
               </v-radio-group>
             </v-col>
 
-            <v-col cols="4">
-              <v-div class="d-flex align-end flex-column">
-                <v-switch class="d-flex align-end flex-column" v-model="neuralSpeech" label="Neural Speech" color="info"
-                  hide-details></v-switch>
-              </v-div>
-            </v-col>
+            <v-col cols="3">
+              <h3>Engine: <div class="custom-tooltip">
+                  <v-badge color="#07A9E7" icon="mdi-exclamation-thick" class="custom-badge">
+                  </v-badge>
+                  <div class="tooltip-content">
+                    <pre class="small-font">
+  Enable this to produce the most 
+  natural and human-like speech 
+  possible. By default it produces 
+  natural-sounding speech.</pre>
+                  </div>
+                </div>
+              </h3>
 
-            <v-col cols="1" class="d-flex align-start flex-column pt-8">
-              <v-badge icon="mdi-exclamation-thick"></v-badge>
+              <div class="switch-label-container">
+                <div class="left-label">Standard</div>
+                <v-switch class="switch-component" v-model="neuralSpeech" color="info" hide-details></v-switch>
+                <div class="right-label">Neural</div>
+              </div>
             </v-col>
 
             <v-col cols="2"></v-col>
@@ -173,7 +183,7 @@
 
             <v-col cols="2" class="d-flex mr-auto flex-column">
               <v-div class="d-flex align-start flex-column">
-                <v-btn :disabled="wordExceed" color="info" @click="speak">Convert</v-btn>
+                <v-btn :disabled="wordExceed" color="info" id="convertButton" @click="speak">Convert</v-btn>
               </v-div>
             </v-col>
             <audio ref="audio"></audio>
@@ -183,8 +193,210 @@
         </v-col>
       </v-row>
     </div>
+
     <div class="mobile-ui" v-else>
-      <p>Hi</p>
+      <v-row class="background pb-10">
+        <v-col cols="12" class="purple lighten-2 d-flex pl-2 shadow1">
+          <v-row>
+            <v-col cols="4" class="align-self-center pr- flex-column">
+              <v-img height="35" width="120" :src="require('@/assets/logo2.png')" class="ml-10"></v-img>
+            </v-col>
+            <v-col cols="6"></v-col>
+            <v-col cols="2" class="align-self-center">
+              <a href="#" @click="logout">
+                <v-avatar color="indigo">
+                  <v-icon color="#FFFFFF">mdi-logout-variant</v-icon>
+                </v-avatar>
+              </a>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col cols="12" class="pt-5">
+          <v-row>
+            <v-col cols="1"></v-col>
+            <v-col cols="10">
+              <h1 class="textShade">Type your words:</h1>
+              <v-textarea id="textarea-no-resize" placeholder="Type the words" rows="5" outlined no-resize
+                background-color="#C0CAF1" border-radius="10px" v-model="text" @input="calculateWordCount" class="mt-5">
+              </v-textarea>
+              <div class="word-count">
+                <h3 :style="{ color: wordCountColor }">
+                  Word Count: {{ wordCount }}
+                </h3>
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col cols="12" color="primary">
+          <v-row>
+            <v-col cols="1"></v-col>
+
+            <v-col cols="4">
+              <h3>Gender:</h3>
+              <v-radio-group v-model="gender" row>
+                <v-radio label="Male" value="male"></v-radio>
+                <v-radio label="Female" value="female"></v-radio>
+              </v-radio-group>
+            </v-col>
+
+            <v-col cols="5">
+              <h3>Engine: <div class="custom-tooltip">
+                  <v-badge color="#07A9E7" icon="mdi-exclamation-thick" class="custom-badge">
+                  </v-badge>
+                  <div class="tooltip-content">
+                    <pre class="small-font">
+  Enable this to produce the most 
+  natural and human-like speech 
+  possible. By default it produces 
+  natural-sounding speech.</pre>
+                  </div>
+                </div>
+              </h3>
+
+              <div class="switch-label-container">
+                <div class="left-label">Standard</div>
+                <v-switch class="switch-component" v-model="neuralSpeech" color="info" hide-details></v-switch>
+                <div class="right-label">Neural</div>
+              </div>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="1"> </v-col>
+            <v-col cols="10">
+              <h3>Language and Region:</h3>
+              <v-combobox v-model="selectedVoice" :items="voiceOptions" label="Voice" item-text="voiceName"
+                item-value="voiceId" @change="handleVoiceChange"></v-combobox>
+            </v-col>
+            <v-col cols="">
+            </v-col>
+          </v-row>
+
+          <v-row class="mb-5">
+            <v-col cols="1"> </v-col>
+
+            <v-col cols="10">
+              <h3>Play Speed：</h3>
+              <v-slider color="#C0CAF1" :ticks="ticks" :tick-labels="speedLabel" max="4" step="1" show-ticks="always"
+                tick-size="4" v-model="sliderValue"></v-slider>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="1"></v-col>
+            <v-col cols="10">
+              <v-progress-linear :value="(currentSeconds / totalSeconds) * 100" :max="totalSeconds"
+                color="primary"></v-progress-linear>
+              <p>{{ currentSeconds }} / {{ totalSeconds }} seconds</p>
+            </v-col>
+            <v-col cols="1"></v-col>
+          </v-row>
+
+          <v-row class="mb-15">
+            <!-- <v-col cols="1"></v-col> -->
+            <v-col cols="1"></v-col>
+            <v-col cols="10">
+              <v-row>
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-btn icon @click="rewind">
+                        <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                          <v-icon color="#FFFFFF">mdi-rewind</v-icon>
+                        </v-avatar>
+                      </v-btn>
+                    </template>
+                  </v-hover>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-btn icon @click="resume">
+                        <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                          <v-icon color="#FFFFFF">mdi-play</v-icon>
+                        </v-avatar>
+                      </v-btn>
+                    </template>
+                  </v-hover>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-btn icon @click="stop">
+                        <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                          <v-icon color="#FFFFFF">mdi-pause</v-icon>
+                        </v-avatar>
+                      </v-btn>
+                    </template>
+                  </v-hover>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-btn icon @click="fastForward">
+                        <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                          <v-icon color="#FFFFFF">mdi-fast-forward</v-icon>
+                        </v-avatar>
+                      </v-btn>
+                    </template>
+                  </v-hover>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-menu offset-y>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-btn icon v-bind="attrs" v-on="on">
+                            <v-avatar :size="38" :color="hover ? 'primary' : 'info'">
+                              <v-icon color="#FFFFFF">mdi-volume-high</v-icon>
+                            </v-avatar>
+                          </v-btn>
+                        </template>
+                        <v-card>
+                          <v-list>
+                            <v-subheader>Volume</v-subheader>
+                            <v-slider v-model="volumeValue" min="0" max="1" step="0.01" thumb-label
+                              @input="updateVolume(volumeValue)"></v-slider>
+                          </v-list>
+                        </v-card>
+                      </v-menu>
+                    </template>
+                  </v-hover>
+                </v-col>
+
+                <v-col cols="2">
+                  <v-hover>
+                    <template v-slot="{ hover }">
+                      <v-btn icon v-if="audioSrc" @click="download">
+                        <v-avatar :size="38" :color="hover ? 'primary' : 'info'"><v-icon
+                            color="#FFFFFF">mdi-download</v-icon></v-avatar></v-btn></template>
+                  </v-hover>
+                </v-col>
+              </v-row>
+
+              <v-row class="mb-15 mt-5">
+                <v-col cols="7"></v-col>
+                <v-col cols="2" class="d-flex mr-auto flex-column">
+                  <v-div class="d-flex align-start flex-column">
+                    <v-btn :disabled="wordExceed" color="info" id="convertButton" @click="speak">Convert</v-btn>
+                  </v-div>
+                </v-col>
+                <audio ref="audio"></audio>
+
+                <v-col cols="3" class="mb-15"></v-col>
+              </v-row>
+            </v-col>
+
+            <v-col cols="1"></v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -209,11 +421,12 @@ export default {
       totalSeconds: 0,
       currentSeconds: 0,
       wordExceed: true,
-      gender: "male",
+      gender: "",
       neuralSpeech: false,
       selectedVoice: "",
-      voiceID: "Joanna",
+      voiceID: "",
       isWeb: false,
+      isConverting: false,
     };
   },
 
@@ -288,7 +501,7 @@ export default {
       const audio = this.$refs.audio;
       audio.pause();
       this.playbackPosition = audio.currentTime; // Store the current playback position
-      audio.currentTime = 0; // Reset the playback position to the beginning
+      // audio.currentTime = 0; // Reset the playback position to the beginning
     },
 
     resume() {
@@ -305,6 +518,9 @@ export default {
     },
 
     speak() {
+      const convertButton = document.getElementById("convertButton");
+      convertButton.disabled = true;
+
       const polly = new AWS.Polly({
         accessKeyId: "",
         secretAccessKey: "",
@@ -316,25 +532,44 @@ export default {
         VoiceId: this.voiceID,
         Engine: this.neuralSpeech ? "neural" : "standard",
       };
+
+      // Check if there is already an audio element playing
+      if (this.$refs.audio) {
+        // Pause the existing audio element
+        this.$refs.audio.pause();
+        this.$refs.audio.currentTime = 0; // Reset the audio to the beginning
+      } else {
+        // Create a new audio element if it doesn't exist
+        this.$refs.audio = new Audio();
+      }
+
+      this.$refs.audio.playbackRate = this.speedLabel[this.sliderValue];
+
       polly.synthesizeSpeech(params, (err, data) => {
-        if (err) console.log(err, err.stack);
-        else {
-          const audio = new Audio(); // Create a new audio element
-          const url = window.URL.createObjectURL(
-            new Blob([data.AudioStream.buffer])
-          );
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          const audio = this.$refs.audio; // Get the audio element
+
+          const url = window.URL.createObjectURL(new Blob([data.AudioStream.buffer]));
           audio.src = url;
           audio.volume = this.volumeValue; // Set the initial volume
           audio.play();
+
           this.audioSrc = url;
+
           audio.addEventListener("loadedmetadata", () => {
             this.totalSeconds = Math.floor(audio.duration);
+            audio.playbackRate = this.speedLabel[this.sliderValue];
+            audio.play();
           });
           audio.addEventListener("timeupdate", () => {
             this.currentSeconds = Math.floor(audio.currentTime);
           });
-          this.$refs.audio = audio; // Assign the audio element to $refs
         }
+
+        // Enable the convert button
+        convertButton.disabled = false;
       });
     },
 
@@ -390,4 +625,65 @@ export default {
 .textShade {
   text-shadow: 1px 1px;
 }
-</style>
+
+.background {
+  background-image: url("https://img.freepik.com/free-vector/lovely-grey-smooth-wave-lines-luxury-background_1017-39363.jpg?w=1060&t=st=1687115336~exp=1687115936~hmac=459d1f32c78265cc9ab1facd55942d94feb95db638d1da668b24dd1c7d9aa574");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.custom-badge {
+  background-color: black;
+}
+
+.custom-badge .v-icon {
+  color: white;
+}
+
+.custom-tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip-content {
+  visibility: hidden;
+  position: absolute;
+
+  transform: translateX(-70%);
+  padding: 8px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 4px;
+  white-space: nowrap;
+  z-index: 1;
+}
+
+.custom-tooltip:hover .tooltip-content {
+  visibility: visible;
+}
+
+.small-font {
+  font-size: 12px;
+}
+
+.switch-label-container {
+  display: flex;
+  align-items: center;
+}
+
+.switch-component {
+  margin: 0 10px;
+  margin-top: 20px;
+}
+
+.left-label,
+.right-label {
+  flex: 1;
+  margin-top: 20px;
+}
+
+.word-count {
+  margin-top: -22px;
+  font-size: 13px;
+}</style>
